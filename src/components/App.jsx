89b95354eb550c964e6c736c512e426e265661b5
Laedinator <laedinator@gmail.com>
2023-01-30 20:56:13 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} key={note.key} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
